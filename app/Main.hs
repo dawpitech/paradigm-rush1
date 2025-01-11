3e04abf005fafdf50007a9edad390484c7e32802
Antoine <me@antoinebellanger.fr>
2025-01-11 13:42:25 +0100

    feat: support input from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,12 @@
 module Main where
 
 import qualified Utils (isSorted, computePS)
-import System.Environment (getArgs)
-import System.Exit
+import System.Environment()
+import System.Exit()
 
 computeExit :: Maybe ([Int], [Int]) -> IO ()
 computeExit Nothing = putStrLn  "OK"
 computeExit (Just tuples) = putStrLn $ "KO: " ++ show tuples
-
-checkArgs :: [String] -> IO()
-checkArgs args | (length args) /= 1 = exitWith (ExitFailure 84)
-checkArgs _ = return ()
 
 checkOutput :: Maybe([Int], [Int]) -> IO()
 checkOutput Nothing = exitWith (ExitFailure 84)
@@ -25,6 +21,5 @@ checkOutput val = computeExit $ Utils.isSorted val
 
 main :: IO ()
 main = do
-    args <- getArgs
-    checkArgs args
-    checkOutput $ Utils.computePS ["sa", "pb", "pb", "pb", "sa", "pa", "pa", "pa"] ([2,1,3,6,5,8], [])
+    instr <- getLine
+    checkOutput $ Utils.computePS (words instr) ([2,1,3,6,5,8], [])
