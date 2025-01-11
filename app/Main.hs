2437b8e1746fb0bc02a357507ded433a8e5d8db8
Antoine <me@antoinebellanger.fr>
2025-01-11 12:24:44 +0100

    fix: split input in array of string

--- app/Main.hs
+++ app/Main.hs
@@ -15,15 +15,7 @@ computeExit :: Maybe ([Int], [Int]) -> IO ()
 computeExit Nothing = putStrLn  "OK"
 computeExit (Just tuples) = putStrLn $ "KO: " ++ show tuples
 
-checkArgs :: [String] -> IO()
-checkArgs args | (length args) /= 1 = exitWith (ExitFailure 84)
-checkArgs _ = return ()
-
 main :: IO ()
 main = do
     args <- getArgs
-    checkArgs args
-    computeExit $ Utils.computePS ["sa", "pb", "pb", "pb", "sa", "pa", "pa", "pa"] ([2,1,3,6,5,8], [])
-    computeExit $ Utils.isSorted $ Just ([6, 5, 8], [3, 2, 1])
-    computeExit $ Utils.isSorted $ Just ([], [1, 2, 3, 4 ,5])
-    computeExit $ Utils.isSorted $ Just ([1,2,3,4,5,6], [])
+    computeExit $ Utils.computePS (words "sa pb pb pb sa pa pa pa") ([2,1,3,6,5,8], [])
